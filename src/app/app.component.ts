1831f24f13f6daee5cae95b1fdaaa10cad5b3d2c
Michael Small <chickenhat22@gmail.com>
2025-01-25 15:09:21 -0600

    feat: add WIP template driven form store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, inject, viewChild } from '@angular/core';
+import { BooksStore } from './template-driven-form.store';
+import { JsonPipe } from '@angular/common';
+import {FormsModule, NgForm} from '@angular/forms';
+import { tap } from 'rxjs';
+import { patchState } from '@ngrx/signals';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [JsonPipe, FormsModule],
+  template: `
+    <form #myForm="ngForm">
+        <input [(ngModel)]="form.firstName" name="firstName" required/>
+        <input [(ngModel)]="form.lastName" name="lastName"/>
+        <input [(ngModel)]="form.signedTOS" type="checkbox" name="signedTOS"/>
+    </form>
+    
+    <pre>{{form.firstName() | json}}</pre>
+    <pre>{{form.lastName() | json}}</pre>
+    <pre>{{form.signedTOS() | json}}</pre>
+    <pre>valid: {{formData().valid}}</pre>
+    <pre>valid in store: {{bookStore.valid()}}</pre>
+  `
 })
 export class AppComponent {
-  title = 'signal-store-forms';
+    bookStore = inject(BooksStore)
+
+    form = this.bookStore.form;
+
+    formData = viewChild.required<NgForm>('myForm');
+
+    syncNgFormMetadataEffect = effect(() => {
+        // this.formData().form.events.pipe(
+        //     tap(() => {
+        //         console.log(this.formData().form)
+        //     })
+        // ).subscribe()
+
+        this.formData().form.statusChanges.pipe(
+            tap((s) => {
+                const _valid = s === 'VALID';
+                this.bookStore.setValidity(_valid)
+            })
+        ).subscribe()
+    })
 }
---
+++ src/app/template-driven-form.store.ts
@@ -0,0 +1,81 @@
+import { computed, inject, Injectable, linkedSignal } from '@angular/core';
+import { debounceTime, distinctUntilChanged, pipe, switchMap, tap } from 'rxjs';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withHooks,
+  withMethods,
+  withProps,
+  withState,
+} from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { tapResponse } from '@ngrx/operators';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({providedIn: 'root'}) export class BooksService {}
+type Book = {
+    title: string,
+    pages: number,
+    author: string,
+}
+
+type BooksState = {
+  books: Book[];
+  isLoading: boolean;
+};
+
+const initialState: BooksState = {
+  books: [],
+  isLoading: false,
+};
+
+export const BooksStore = signalStore(
+  {providedIn: 'root'},
+  withState(initialState),
+  withComputed(({ books }) => ({
+    booksCount: computed(() => books().length),
+  })),
+  withMethods((store, booksService = inject(BooksService)) => ({})),
+  withProps((store) => {
+    const _firstName = linkedSignal(() => '');
+    const _lastName = linkedSignal(() => '');
+    const _signedTOS = linkedSignal({
+        source: () => ({fn: _firstName(), ln: _lastName()}),
+        computation: (src, prev) => {
+            console.log(prev?.source.fn)
+            if (prev && prev.source.fn !== src.fn && prev.source.ln !== src.ln) {
+                return false
+            } else {
+                return false
+            }
+        }
+    })
+
+    const _form = {
+        firstName: _firstName,
+        lastName: _lastName,
+        signedTOS: _signedTOS
+    }
+    return {
+        form: _form
+    }
+  }),
+  withState({valid: false}),
+  withMethods((store) => ({
+    setValidity(v: boolean): void {
+        patchState(store, (state) => ({valid: v}))
+    }
+  })),
+  withHooks((store) => {
+    const stuff = 'd';
+    return {
+        onInit() {
+            console.log('init', stuff)
+        },
+        onDestroy() {
+            console.log('onDestroy')
+        }
+    }
+  })
+);
